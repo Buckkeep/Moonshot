c9e555ec9568b85faf48cb9b95bec3fa13e5f1c8
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-23 13:59:16 +0100

    Replaced decoding guard with do-catch error handling

--- Moonshot/Bundle-Decodable.swift
+++ Moonshot/Bundle-Decodable.swift
@@ -19,10 +19,18 @@ extension Bundle {
         
         let decoder = JSONDecoder()
         
-        guard let loaded = try? decoder.decode([String: Astronaut].self, from: data) else {
-            fatalError("Failed to decode \(file) from bundle")
+        do {
+            return try decoder.decode([String:Astronaut].self, from: data)
+        } catch DecodingError.keyNotFound(let key, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing key '\(key.stringValue)' - \(context.debugDescription)")
+        } catch DecodingError.typeMismatch(_, let context) {
+            fatalError("Failed to decode \(file) from bundle due to type mismatch - \(context.debugDescription)")
+        } catch DecodingError.valueNotFound(let type, let context) {
+            fatalError("Failed to decode \(file) from bundle due to missing \(type) value - \(context.debugDescription)")
+        } catch DecodingError.dataCorrupted(_) {
+            fatalError("Failed do decode \(file) from the bundle because it appears to be invalid JSON")
+        } catch {
+            fatalError("Failed to decode \(file) from the bundle: \(error.localizedDescription)")
         }
-        
-        return loaded
     }
 }
