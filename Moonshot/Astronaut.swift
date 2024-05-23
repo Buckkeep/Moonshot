b71938b4d16631bc6a1bf8ff5c254f032d392a65
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-23 13:50:29 +0100

    Basic starter decoding

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 23/05/2024.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
