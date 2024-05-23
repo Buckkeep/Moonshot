33c9c1e27ad7736f441c644d9af27d68c7b567bd
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-05-23 11:43:07 +0100

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Buhecha, Neeta (Trainee Engineer) on 23/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
