8e0746d513c09e5f69f98816caea08c41761be0b
Neeta Buhecha <neeta.buhecha@natwest.com>
2024-07-01 11:00:59 +0100

    Refactoring LazyVGrid to avoid repetition

--- Moonshot/MixedView.swift
+++ Moonshot/MixedView.swift
@@ -21,39 +21,7 @@ struct MixedView: View {
     ]
     
     private var lazyGridView: some View {
-        LazyVGrid(columns: columns) {
-            ForEach(missions) {mission in
-                NavigationLink {
-                    MissionView(mission: mission, astronauts: astronauts)
-                } label: {
-                    VStack {
-                        Image(mission.image)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 100, height: 100)
-                            .padding()
-                        VStack {
-                            Text(mission.displayName)
-                                .font(.headline)
-                                .foregroundStyle(.white)
-                            
-                            Text(mission.formattedLaunchDate)
-                                .font(.caption)
-                                .foregroundStyle(.white.opacity(0.6))
-                            
-                        }
-                    .padding(.vertical)
-                    .frame(maxWidth: .infinity)
-                    .background(.lightBackground)
-                    }
-                    .clipShape(.rect(cornerRadius: 10))
-                    .overlay(
-                    RoundedRectangle(cornerRadius: 10)
-                        .stroke(.lightBackground)
-                    )
-                }
-            }
-        }
+        LazyVGrid(columns: columns) {listView}
     }
     
     private var listView: some View {
